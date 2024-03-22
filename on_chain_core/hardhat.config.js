2f0ba1d69960439d4fa1ee2ee37b8e5e2b8e5f57
masato25 <leomasato25@gmail.com>
2024-03-23 03:10:21 +0800

    chore: update conf

--- on_chain_core/hardhat.config.js
+++ on_chain_core/hardhat.config.js
@@ -1,7 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
+const alchemyKey = process.env.ALCHEMY_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://arb-mainnet.g.alchemy.com/v2/${alchemyKey}`,
+        blockNumber: 193100268
+      },
+    },
+    hh: {
+      url: `http://0.0.0.0:8545`,
+      // accounts: {
+      //   mnemonic: mnemonic,
+      // },
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", process.env.ETH_PRIVATE_KEY],
+      timeout: 1400000
+    },
+  },
   solidity: {
     compilers: [
       {

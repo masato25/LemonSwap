43dc933ebf664854656468cde29b7d66e59d4b9c
masato25 <leomasato25@gmail.com>
2024-03-23 20:13:28 +0800

    chore: update test script

--- on_chain_core/scripts/deploy.js
+++ on_chain_core/scripts/deploy.js
@@ -24,17 +24,19 @@ async function main() {
     await contractInstance.init("0x0000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000");
 
     const ARBToken = await hre.ethers.getContractAt("IERC20", "0x912CE59144191C1204E64559FE8253a0e49E6548");
-    console.log(accounts[1]);
-    const tx = await ARBToken.connect(mainAccount).approve(contractAddr, "1952427722501507843127");
+    const balanceAmount = await ARBToken.balanceOf(mainAccount.address);
+    console.log(accounts[1], balanceAmount);
+    const tx = await ARBToken.connect(mainAccount).approve(contractAddr, balanceAmount);
     const receipt = await tx.wait();
     // console.log(receipt.logs);
     console.log(await ARBToken.allowance(mainAccount.address, contractAddr));
-    await contractInstance.connect(mainAccount).openPosition(["0xc6f780497a95e246eb9449f5e4770916dcd6396a", false, "1952427722501507843127", "76410", "76310" ]);
+    await contractInstance.connect(mainAccount).openPosition(["0xc6f780497a95e246eb9449f5e4770916dcd6396a", false, balanceAmount, "76410", "76310" ]);
     console.log(await ARBToken.balanceOf(mainAccount.address));
     const positionId = await contractInstance.connect(mainAccount).userPositionIds(mainAccount.address, 0);
     await contractInstance.connect(mainAccount).closePosition(positionId);
     // await contractInstance.closePosition(positionId);
     console.log(await ARBToken.balanceOf(mainAccount.address));
+    console.log(await contractInstance.totalPositions());
   } catch (error) {
     console.error(error);
     process.exit(1);
